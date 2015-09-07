ff66833c131e4ac202d61b1f336d40c5a3b98b88
Junne <junne.bai@gmail.com>
2015-09-07 23:47:07 +0800

    add two view

---
+++ DayStep/View/BarChartView.swift
@@ -0,0 +1,21 @@
+//
+//  BarChartView.swift
+//  DayStep
+//
+//  Created by Junne on 9/7/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+import UIKit
+
+class BarChartView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DayStep/View/LineChartView.swift
@@ -0,0 +1,21 @@
+//
+//  LineChartView.swift
+//  DayStep
+//
+//  Created by Junne on 9/7/15.
+//  Copyright (c) 2015 Junne. All rights reserved.
+//
+
+import UIKit
+
+class LineChartView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
